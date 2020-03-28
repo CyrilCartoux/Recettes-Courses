5680e0a37adfbf76946df3cb9fae0920b42b915c
Cyril Cartoux <6ssou@protonmail.com>
2020-03-28 17:12:31 +0100

    Structure arrangements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shoppingList/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shoppingList/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.
     ShoppingListEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
