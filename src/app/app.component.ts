25fc9abde9178eb4fe588a90b698388ca7c29d43
Cyril Cartoux <6ssou@protonmail.com>
2020-04-04 12:07:17 +0200

    naviguation with event binding, passing recipe data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courseProject';
+  loadedFeature = 'recipe';
+
+  onNaviguate(feature: string) {
+   this.loadedFeature = feature;
+  }
+
 }
